6f562a15051422e6fafe82f6d538076a81d1f488
Jomo <jomomg@gmail.com>
2017-03-01 11:43:23 +0300

    Add docstrings to the classes

--- tests.py
+++ tests.py
@@ -3,12 +3,19 @@ from classes import Fellow, Staff, Office, LivingSpace
 
 
 class RoomTests(unittest.TestCase):
+    """Tests to ensure proper living space and office creation
+
+     These tests assume that the 'all_offices' and 'all_living_spaces' methods return a list.
+     Hence the 'count' method of the list object is used to test
+     Whether a new office  or a new living space exists in the list returned. """
+
     def setUp(self):
         self.new_office = Office()
         self.new_living_space = LivingSpace()
 
     def test_create_new_office(self):
         self.new_office.create_new("Test")
+
         self.assertEqual(self.new_office.all_offices.count("Test"), 1,
                          "A new office was not created")
 
@@ -32,6 +39,12 @@ class RoomTests(unittest.TestCase):
 
 
 class PersonTests(unittest.TestCase):
+    """Tests to ensure proper fellow and staff addition
+
+     These tests assume that the 'all_staff' and 'all_fellows' methods return a list.
+     Hence the 'count' method of the list object is used to test
+     Whether a new fellow  or a new staff member exists in the list returned. """
+
     def setUp(self):
         self.new_fellow = Fellow()
         self.new_staff = Staff()
