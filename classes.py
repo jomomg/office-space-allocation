32ab264565f99281035bab83d047400fdb92ea68
Jomo <jomomg@gmail.com>
2017-03-03 11:38:47 +0300

    Add methods for allocating offices and living spaces randomly

--- classes.py
+++ classes.py
@@ -1,4 +1,8 @@
 from models import Model
+import random
+
+MAX_OFFICE_CAP = 6
+MAX_LIVING_SPACE_CAP = 4
 
 
 class Dojo:
@@ -6,14 +10,88 @@ class Dojo:
         self.name = None
         self.current_capacity = 0
 
-    def allocate(self):
-        pass
+    @staticmethod
+    def allocate_office_space(person_name, person_type):
+        """ This methods randomly allocates an office to a fellow or staff.
+
+            Returns True if successful and False if not
+        """
+
+        successful = True
+        random.seed()
+
+        # return the list of available offices
+        offices = Model.return_list(list_to_be_returned="offices")
+        # return a list of offices with capacity to spare
+        non_full_offices = [x for x in offices if x[1] < MAX_OFFICE_CAP]
+
+        if person_type == "fellow":
+            # get a random office from the list of non full offices
+            random_office = random.choice(non_full_offices)
+            # get the fellow with the given name
+            fellow = Model.get_fellow(person_name)
+            # increment the current capacity of the chosen office by 1
+            random_office[1] += 1
+            # add the office name to be the fellow's current office
+            fellow[1] = random_office[0]
+            return successful
+
+        elif person_type == "staff":
+            # get a random office from the list of non full offices
+            random_office = random.choice(non_full_offices)
+            # get the staff member with the given name
+            staff = Model.get_staff(person_name)
+            # increment the current capacity of the chosen office by 1
+            random_office[1] += 1
+            # add the office name to be the staff member's current office
+            staff[1] = random_office[0]
+            return successful
+
+        else:
+            return not successful
+
+    @staticmethod
+    def allocate_living_space(fellow_name):
+        """ This methods randomly allocates a living space to a fellow or staff.
+
+            Returns True if successful and False if not
+        """
+
+        successful = True
+
+        # return a list of available living spaces
+        living_spaces = Model.return_list(list_to_be_returned="living_spaces")
+        # return a list of living spaces with capacity to spare
+        non_full_living_spaces = [x for x in living_spaces if x[1] < MAX_OFFICE_CAP]
+        # get a random living space
+        random_living_space = random.choice(non_full_living_spaces)
+        # get the fellow specified
+        fellow = Model.get_fellow(fellow_name)
+        # increment the current capacity of the living space by 1
+        random_living_space[1] += 1
+        # add the living space's name to the fellows current living space
+        fellow[2] = random_living_space[0]
+
+        return successful
 
 
 class Person:
     def __init__(self):
         self.name = None
         self.current_office = None
+
+    def get_current_office(self, person_name, person_type):
+        """ Returns the current office of the specified person"""
+
+        if person_type == "fellow":
+            fellow = Model.get_fellow(person_name)
+            self.current_office = fellow[1]
+            return self.current_office
+
+        elif person_type == "staff":
+            staff = Model.get_staff(person_name)
+            self.current_office = staff[1]
+            return self.current_office
 
 
 class Fellow(Person):
@@ -37,6 +115,12 @@ class Fellow(Person):
         """ Returns a list containing all the fellows added """
 
         return Model.return_list(list_to_be_returned="fellows")
+
+    def get_current_living_space(self, fellow_name):
+        """ Returns the current living space of the specified fellow"""
+
+        fellow = Model.get_fellow(fellow_name)
+        self.current_living_space = fellow[2]
 
 
 class Staff(Person):
@@ -71,6 +155,7 @@ class Office(Dojo):
     def create_new(self, new_office):
         """ Creates a new office """
 
+        successful = True
         if not isinstance(new_office, str):
             raise TypeError("Input is not a string")
 
@@ -84,6 +169,8 @@ class Office(Dojo):
             if entry.count(self.name) == 1:
                 raise ValueError("Office already exists")
         self.model.update(new_entry, list_to_be_appended="offices")
+        return successful
+
 
     @property
     def all_offices(self):
@@ -100,6 +187,7 @@ class LivingSpace(Dojo):
     def create_new(self, new_living_space):
         """ Creates a new living space """
 
+        successful = True
         self.name = new_living_space
         new_entry = [self.name, self.current_capacity]
         living_spaces = Model.return_list(list_to_be_returned="living_spaces")
@@ -110,10 +198,13 @@ class LivingSpace(Dojo):
                 raise ValueError("Living space already exists")
         # create new living space
         self.model.update(new_entry, list_to_be_appended="living_spaces")
+        return successful
 
     @property
     def all_living_spaces(self):
         """ Return a list containing all the living spaces created """
 
         return Model.return_list(list_to_be_returned="living_spaces")
+
+
 
