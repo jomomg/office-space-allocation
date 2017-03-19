a0e9ac728967e3e4d01ca545434f09aa83c544e0
Jomo <jomomg@gmail.com>
2017-03-19 21:43:16 +0300

    Change allocate and create methods to use a return statement instead of raising an exception

--- app/dojo.py
+++ app/dojo.py
@@ -27,7 +27,6 @@ class Dojo:
             Returns True if successful and False if not
         """
 
-        successful = True
         random.seed()
 
         # return the list of available offices
@@ -37,24 +36,24 @@ class Dojo:
                             for office in all_offices
                             if office.current_capacity < MAX_OFFICE_CAP]
         if not non_full_offices:
-            raise ValueError("There are no offices to allocate")
+            return "There are no offices to allocate"
 
         if person_type == "fellow":
             random_office = random.choice(non_full_offices)
             fellow = md.Model.get_fellow(person_name, email)
             random_office.current_capacity += 1
             fellow.current_office = random_office.name
-            return successful
+            return "Fellow {} has been allocated the office {}".format(fellow.name, fellow.current_office)
 
         elif person_type == "staff":
             random_office = random.choice(non_full_offices)
             staff = md.Model.get_staff(person_name, email)
             random_office.current_capacity += 1
             staff.current_office = random_office.name
-            return successful
+            return "Staff {} has been allocated the living space {}".format(staff.name, staff.current_office)
 
         else:
-            return not successful
+            return "Unable to allocate an office"
 
     @staticmethod
     def allocate_living_space(fellow_name, email):
@@ -62,57 +61,47 @@ class Dojo:
             Returns True if successful and False if not
         """
 
-        successful = True
         random.seed()
-
-        # return a list of available living spaces
         all_living_spaces = md.Model.get_list("living_spaces")
-        # return a list of living spaces with capacity to spare
         non_full_living_spaces = [living_space
                                   for living_space in all_living_spaces
                                   if living_space.current_capacity < MAX_LIVING_SPACE_CAP]
 
         if not non_full_living_spaces:
-            raise ValueError("There are no living spaces to allocate")
+            return "There are no living spaces to allocate"
         random_living_space = random.choice(non_full_living_spaces)
         fellow = md.Model.get_fellow(fellow_name, email)
         random_living_space.current_capacity += 1
         fellow.current_living_space = random_living_space.name
-
-        return successful
+        return "Fellow {} has been allocated the living space {}".format(fellow.name, fellow.current_living_space)
 
     def create_new_office(self, name):
         """ Creates a new office """
 
-        successful = True
         new_office = room.Office(name)
-
-        # get the existing offices
         offices = md.Model.get_list("offices")
         living_spaces = md.Model.get_list("living_spaces")
-        # check whether the office already exists, if it does raise an exception
+
         similar_office = [office for office in offices if office.name == name]
         similar_living_space = [ls for ls in living_spaces if ls.name == name]
         if similar_office or similar_living_space:
-            raise ValueError("An office or living space '{}' already exists".format(name))
+            return "An office or living space '{}' already exists".format(name)
         self.model.update(new_office, "offices")
-        return successful
+        return "An office called '{}' has been successfully created".format(new_office.name)
 
     def create_new_living_space(self, name):
         """ Creates a new living space """
 
-        successful = True
         new_living_space = room.LivingSpace(name)
         living_spaces = md.Model.get_list("living_spaces")
         offices = md.Model.get_list("offices")
-        # check whether the living space already exists, if it does raise an exception
         similar_living_space = [ls for ls in living_spaces if ls.name == name]
         similar_office = [office for office in offices if office.name == name]
         if similar_living_space or similar_office:
-            raise ValueError("An office or living space '{}' already exists".format(name))
+            return "An office or living space '{}' already exists".format(name)
         # create new living space
         self.model.update(new_living_space, "living_spaces")
-        return successful
+        return "A living space called '{}' has been successfully created".format(new_living_space.name)
 
     def add_fellow(self, name, email, wants_accommodation=False):
         """ Adds a new fellow """
@@ -121,24 +110,14 @@ class Dojo:
         fellows = self.model.get_list("fellows")
         similar = [fellow for fellow in fellows if fellow.email_address == email]
         if similar:
-            return "The email address provided already exists"
-
-        self.model.update(new_fellow, "fellows")
-        print("Fellow {} has been successfully added".format(new_fellow.name))
-        try:
-            self.allocate_office_space(new_fellow.name, email, person_type="fellow")
-            print("{} has been allocated the office {}"
-                  .format(new_fellow.name, new_fellow.current_office))
-        except ValueError as e:
-            print(e)
+            print("The email address provided already exists")
+        else:
+            self.model.update(new_fellow, "fellows")
+            print("Fellow {} has been successfully added".format(new_fellow.name))
+            print(self.allocate_office_space(new_fellow.name, email, person_type="fellow"))
 
         if wants_accommodation:
-            try:
-                self.allocate_living_space(new_fellow.name, email)
-                print("{} has been allocated the living space {}"
-                      .format(new_fellow.name, new_fellow.current_living_space))
-            except ValueError as e:
-                print(e)
+            print(self.allocate_living_space(new_fellow.name, email))
 
     def add_staff(self, name, email):
         """ Adds a new member of staff """
@@ -147,16 +126,11 @@ class Dojo:
         staff = self.model.get_list("staff")
         similar = [staff_member for staff_member in staff if staff_member.email_address == email]
         if similar:
-            return "The email address provided already exists"
-
-        self.model.update(new_staff, "staff")
-        print("Staff {} has been successfully added".format(new_staff.name))
-        try:
-            self.allocate_office_space(new_staff.name, email, person_type="staff")
-            print("{} has been allocated the office {}"
-                  .format(new_staff.name, new_staff.current_office))
-        except ValueError as e:
-            print(e)
+            print("The email address provided already exists")
+        else:
+            self.model.update(new_staff, "staff")
+            print("Staff {} has been successfully added".format(new_staff.name))
+            print(self.allocate_office_space(new_staff.name, email, person_type="staff"))
 
     def get_persons_by_room(self, room):
         """Returns a list of the people in the given room"""
@@ -170,7 +144,7 @@ class Dojo:
         persons_in_room = []
         for person in persons:
             if person.current_office == room or person.current_living_space == room:
-                persons_in_room.append(person.name)
+                persons_in_room.append("{} {}".format(person.email_address, person.name))
         return persons_in_room
 
     def print_persons_by_room(self, room_name):
@@ -197,15 +171,15 @@ class Dojo:
 
         for fellow in fellows:
             if ("L_SPACE", fellow.current_living_space) not in rooms:
-                rooms[("L_SPACE", fellow.current_living_space)] = [fellow.name]
+                rooms[("L_SPACE", fellow.current_living_space)] = ["{} {}".format(fellow.email_address, fellow.name)]
             else:
-                rooms[("L_SPACE", fellow.current_living_space)] += [fellow.name]
+                rooms[("L_SPACE", fellow.current_living_space)] += ["{} {}".format(fellow.email_address, fellow.name)]
 
         for person in itertools.chain(staff, fellows):
             if ("OFFICE", person.current_office) not in rooms:
-                rooms[("OFFICE", person.current_office)] = [person.name]
+                rooms[("OFFICE", person.current_office)] = ["{} {}".format(person.email_address, person.name)]
             else:
-                rooms[("OFFICE", person.current_office)] += [person.name]
+                rooms[("OFFICE", person.current_office)] += ["{} {}".format(person.email_address, person.name)]
 
         if not rooms:
             raise ValueError("No allocations have been made".upper())
@@ -227,7 +201,7 @@ class Dojo:
                 if room[1]:
                     output += "({}) {}:\n".format(room[0], room[1].upper()) + \
                               "-----------------------------\n" + \
-                              "{}\n\n".format(",".join(map(str.upper, occupants)))
+                              "{}\n\n".format("\n".join(occupants))
 
             return output
 
@@ -260,13 +234,13 @@ class Dojo:
 
         output = "STAFF WITHOUT AN OFFICE:\n" \
                  "----------------------------\n" + \
-                 "{}\n\n".format(", ".join(map(str.upper,unallocated["staff"]))) + \
+                 "{}\n\n".format(", ".join(map(str.upper, unallocated["staff"]))) + \
                  "FELLOWS WITHOUT AN OFFICE:\n" \
                  "-----------------------------\n" + \
-                 "{}\n\n".format(", ".join(map(str.upper,unallocated["fellows_office"]))) + \
+                 "{}\n\n".format(", ".join(map(str.upper, unallocated["fellows_office"]))) + \
                  "FELLOWS WITHOUT A LIVING SPACE:\n" \
                  "------------------------------\n" + \
-                 "{}\n\n".format(", ".join(map(str.upper,unallocated["fellows_living"])))
+                 "{}\n\n".format(", ".join(map(str.upper, unallocated["fellows_living"])))
 
         if filename:
             with open(filename + ".txt", "w") as file:
@@ -377,7 +351,6 @@ class Dojo:
     def load_from_database(self, session):
         success = True
         all_fellows = session.query(md.Fellow).all()
-        print(all_fellows)
         for fellow in all_fellows:
 
             new_fellow = person.Fellow(fellow.name, fellow.email)
